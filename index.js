df1415c0a50c1daa6d36b951094b1cb4ac234b78
MD Biplop Hossain <developerbiplop@gmail.com>
2022-05-06 22:57:59 +0600

    server setup, get api for product and inventory route api

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ddxnu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    await client.connect();
+    const productsCollection = client.db('bikewarehouse').collection('productCollection');
+    app.get('/products', async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    app.get('/inventory/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    })
+  }
+  finally {
+    // finally
+  }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send("Running BikeWarehouse Server is Running")
+})
+
+app.listen(port, () => {
+  console.log('Listening to port', port)
+})
